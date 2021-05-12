c84776f63d098e4bc45eef794775098f6daf29c9
builtbyjosh <pc.refuge@gmail.com>
2021-05-12 11:24:50 -0500

    added show route for breweries

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -3,4 +3,9 @@ class BreweriesController < ApplicationController
     breweries = Brewery.all
     render json: breweries.to_json(:include => [:reviews])
   end
+
+  def show
+    brewery = Brewery.find_by(params[:id])
+    render json: brewery.to_json(:include => [:reviews])
+  end
 end
